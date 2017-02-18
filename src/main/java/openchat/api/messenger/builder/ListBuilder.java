3ce524b4029b55a4c5539996c96e5dc2479389bb
vgorin <vgrn.at@gmail.com>
2017-02-18 18:03:31 +0200

    ListBuilder improvements

--- src/main/java/openchat/api/messenger/builder/ListBuilder.java
+++ src/main/java/openchat/api/messenger/builder/ListBuilder.java
@@ -16,6 +16,11 @@ import java.util.*;
 
 
 public class ListBuilder extends MessageBuilder {
+	public static final int COMPACT_PAGE_SIZE = 4;
+	public static final int LARGE_PAGE_SIZE = 3;
+
+	private int capacity;
+
 	public ListBuilder() {
 		this("compact");
 	}
@@ -31,22 +36,15 @@ public class ListBuilder extends MessageBuilder {
 		message.attachment.payload = new Payload("list");
 		message.attachment.payload.topElementStyle = topElementStyle;
 		message.attachment.payload.elements = capacity > 0? new ArrayList<>(capacity): new LinkedList<>();
+		this.capacity = capacity;
 	}
 
 	public static ListBuilder compact() {
-		return new ListBuilder("compact");
-	}
-
-	public static ListBuilder compact(int capacity) {
-		return new ListBuilder("compact", capacity);
+		return new ListBuilder("compact", COMPACT_PAGE_SIZE);
 	}
 
 	public static ListBuilder large() {
-		return new ListBuilder("large");
-	}
-
-	public static ListBuilder large(int capacity) {
-		return new ListBuilder("large", capacity);
+		return new ListBuilder("large", LARGE_PAGE_SIZE);
 	}
 
 	public ListItemBuilder createElement(String title) {
@@ -66,11 +64,17 @@ public class ListBuilder extends MessageBuilder {
 	}
 
 	public ListBuilder addElement(Element element) {
-		message.attachment.payload.elements.add(element);
-		return this;
+		if(message.attachment.payload.elements.size() < capacity) {
+			message.attachment.payload.elements.add(element);
+			return this;
+		}
+		throw new IllegalStateException(String.format("too many elements, maximum is %d", capacity));
 	}
 
 	public ListBuilder addElements(Collection<Element> elements) {
+		if(message.attachment.payload.elements.size() + elements.size() > capacity) {
+			throw new IllegalStateException(String.format("too many elements, maximum is %d", capacity));
+		}
 		message.attachment.payload.elements.addAll(elements);
 		return this;
 	}
@@ -88,6 +92,14 @@ public class ListBuilder extends MessageBuilder {
 		}
 		message.attachment.payload.buttons.add(button);
 		return this;
+	}
+
+	public int size() {
+		return message.attachment.payload.elements.size();
+	}
+
+	public int capacity() {
+		return capacity;
 	}
 
 }
