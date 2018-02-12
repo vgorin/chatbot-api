50adf2c8b74220b84c985ddbed82a588cde51591
vgorin <vgrn.at@gmail.com>
2018-02-12 21:40:49 +0200

    do not fail on unknown JSON properties

--- src/main/java/openchat/util/JsonUtil.java
+++ src/main/java/openchat/util/JsonUtil.java
@@ -3,6 +3,7 @@ package openchat.util;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
@@ -24,6 +25,8 @@ public class JsonUtil {
 		OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 		// allow reading of JSON values without quotes
 		OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		// do not fail on unknown JSON properties
+		OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		// support both Jackson and JAXB annotations
 		OBJECT_MAPPER.setAnnotationIntrospector(new AnnotationIntrospectorPair(new JacksonAnnotationIntrospector(), new JaxbAnnotationIntrospector()));
 	}
